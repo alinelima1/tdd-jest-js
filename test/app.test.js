8d8ae42a7b6bf091d6f054a358a673ceb5f11778
Aline de Souza Lima Abreu <alineslima17@gmail.com>
2020-05-03 10:14:51 -0300

    add test

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,33 @@
+const calculadora = require('../src/js/app')
+
 describe("calculadora", () => {
-    it
+    it('Teste exemplo', () => {
+        expect(23 + 7).toBe(30);
+        expect(10).toBe(10);
+        expect(23 + 7).not.toBe(31);
+    });
+
+    describe('Teste de soma', () => {
+        it('Retorna 3 quando 2 + 1', () => {
+            expect(calculadora.soma(2, 1)).toBe(3)
+        })
+    });
+
+    describe('Teste de subtração', () => {
+        it('Retorna 3 quando 4 - 1', () => {
+            expect(calculadora.subtracao(4, 1)).toBe(3)
+        })
+    });
+
+    describe('Teste de multiplicação', () => {
+        it('Retorna 4 quando 2 * 2', () => {
+            expect(calculadora.multiplicacao(2,2)).toBe(4)
+        })
+    });
+
+    describe('Teste de divisão', () => {
+        it('Retorna 2 quando 4 / 2', () => {
+            expect(calculadora.divisao(4,2)).toBe(2)
+        })
+    });
 })
